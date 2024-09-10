804cec7f0de5a4b8958e67a825d70b50ef34c17d
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2024-09-10 23:33:00 +0900

    feat: Chat History

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,10 +1,13 @@
-import time
+import os
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
 from langchain.storage import LocalFileStore
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
 import streamlit as st
+from dotenv import load_dotenv
+
+load_dotenv(".././env/.env")
 
 st.set_page_config(
     page_title="DocumentGPT",
@@ -12,24 +15,44 @@ st.set_page_config(
 )
 
 
+# 파일이 동일하다면, 캐싱된 데이터를 사용한다.
+# show_spinner는 캐싱된 데이터를 사용할 때, 스피너를 보여줄지 여부를 결정한다.
+@st.cache_data(show_spinner="Embedding file...")
 def embed_file(file):
+    base_dir = os.path.abspath(".")
     file_content = file.read()
-    file_path = f"./.cache/files/{file.name}"
+    file_path = f"{base_dir}/../.cache/files/{file.name}"
     with open(file_path, "wb") as f:
         f.write(file_content)
-    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    cache_dir = LocalFileStore(f"{base_dir}/../.cache/embeddings/{file.name}")
     splitter = CharacterTextSplitter.from_tiktoken_encoder(
         separator="\n",
         chunk_size=600,
         chunk_overlap=100,
     )
-    loader = UnstructuredFileLoader("./files/chapter_one.txt")
+    loader = UnstructuredFileLoader(file_path)
     docs = loader.load_and_split(text_splitter=splitter)
     embeddings = OpenAIEmbeddings()
     cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
     vectorstore = FAISS.from_documents(docs, cached_embeddings)
     retriever = vectorstore.as_retriever()
     return retriever
+
+
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.markdown(message)
+    if save:
+        st.session_state["messages"].append({"message": message, "role": role})
+
+
+def paint_history():
+    for message in st.session_state["messages"]:
+        send_message(
+            message["message"],
+            message["role"],
+            save=False,
+        )
 
 
 st.title("DocumentGPT")
@@ -39,14 +62,23 @@ st.markdown(
 Welcome!
             
 Use this chatbot to ask questions to an AI about your files!
+
+Upload your files on the sidebar.
 """
 )
 
-file = st.file_uploader(
-    "Upload a .txt .pdf or .docx file",
-    type=["pdf", "txt", "docx"],
-)
+with st.sidebar:
+    file = st.file_uploader(
+        "Upload a .txt .pdf or .docx file",
+        type=["pdf", "txt", "docx"],
+    )
 
 if file:
     retriever = embed_file(file)
-    s = retriever.invoke("winston")
+    send_message("I'm ready! Ask away!", "ai", save=False)
+    paint_history()
+    message = st.chat_input("Ask anything about your file...")
+    if message:
+        send_message(message, "human")
+else:
+    st.session_state["messages"] = []
